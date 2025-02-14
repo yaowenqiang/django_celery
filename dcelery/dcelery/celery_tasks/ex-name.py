45faaf1770fba6ba6b119180d11e48a18994d11e
yaowenqiang <yaowenqiang111@163.com>
2025-02-14 23:48:56 +0800

    add task except handling demo

--- dcelery/dcelery/celery_tasks/ex-name.py
+++ dcelery/dcelery/celery_tasks/ex-name.py
@@ -1,6 +1,18 @@
 from dcelery.celery_config import app
+import logging
+
+logging.basicConfig(filename='celery.log', level=logging.ERROR, format='%(asctime)s %(levelname)s %(message)s')
+
 
 
 @app.task(queue='tasks'):
 def my_task():
     pass
+
+@app.task(queue='tasks'):
+def my_task2():
+    try:
+        raise ConnectionError('Connection Error')
+    except ConnectionError as e:
+        logging.error(e)
+        raise ConnectionError()
