4bc5bfbe5fefa2b90a8e5540697afc9d38df030d
yaowenqiang <yaowenqiang111@163.com>
2025-02-15 23:39:08 +0800

    add custome error class demo

---
+++ dcelery/dcelery/celery_tasks/ex-custom_class.py
@@ -0,0 +1,26 @@
+from dcelery.celery_config import app
+from celery import Task
+import logging
+
+logging.basicConfig(filename='celery.log', level=logging.ERROR, format='%(asctime)s %(levelname)s %(message)s')
+
+class CustomClass(Task):
+    def on_failure(self, exc, task_id, args, kwargs, einfo):
+        if isinstance(exc, ConnectionError):
+            logging.error(einfo)
+        else:
+            print('{0!r} failed: {1!r}'.format(task_id, exc))
+
+app.Task = CustomClass()
+
+@app.task(queue='tasks')
+def my_task():
+    pass
+
+@app.task(queue='tasks')
+def my_task2():
+    try:
+        raise ConnectionError('Connection Error')
+    except ConnectionError as e:
+        logging.error(e)
+        raise ConnectionError()
