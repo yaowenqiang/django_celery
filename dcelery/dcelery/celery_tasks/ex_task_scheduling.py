0f8affcbcd5b963b51bf07a629dbfbed88530e80
yaowenqiang <yaowenqiang111@163.com>
2025-02-24 23:40:39 +0800

    add task schedule demo

---
+++ dcelery/dcelery/celery_tasks/ex_task_scheduling.py
@@ -0,0 +1,28 @@
+
+"""
+celery --app=dcelery worker -l INFO -Q tasks -E -B
+B for beat
+E for events
+"""
+
+from datetime import datetime, timedelta
+from dcelery.celery_config import app
+
+app.conf.beat_schedule = {
+    'task1': {
+        'task': dcelery.celery_tasks.ex_task_scheduling.task1,
+        'schedule': timedelta(seconds=5),
+    },
+    'task2': {
+        'task': dcelery.celery_tasks.ex_task_scheduling.task2,
+        'schedule': timedelta(seconds=10),
+    },
+}
+
+@app.task
+def task1(queue='tasks'):
+    print('Running task 1')
+
+@app.task
+def task2(queue='tasks'):
+    print('Running task 2')
